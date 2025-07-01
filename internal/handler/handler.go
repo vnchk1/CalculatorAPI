3e31f4a78bbf66b60572f8f346868b4433b8231a
Ivan Cherviakov <ivanchervyakov2905@gmail.com>
2025-07-02 00:04:42 +0300

    String uuid

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -23,7 +23,7 @@ func NewHandler(logger *slog.Logger, storage *store.SafeMap) *Handler {
 
 func (h *Handler) SumHandler(c echo.Context) error {
 	var req models.NumbersRequest
-	id := uuid.New()
+	id := uuid.New().String()
 
 	if err := c.Bind(&req); err != nil {
 		h.logger.Error("error with parsing JSON", "error", err)
